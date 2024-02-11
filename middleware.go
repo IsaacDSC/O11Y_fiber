87b1b201915d41a0b894353229504a63e99466ca
IsaacDSC <isaac8.silva@hotmail.com>
2024-02-11 15:01:02 -0300

    feat: add exclude paths in io middleware

--- middleware.go
+++ middleware.go
@@ -8,36 +8,52 @@ import (
 	"github.com/google/uuid"
 )
 
-func MiddlewareIO(c *fiber.Ctx) error {
-	l := NewLogger()
-	var (
-		correlationID = uuid.New().String()
-		requestID     = uuid.New().String()
-	)
-	ctx := context.WithValue(c.UserContext(), "correlation_id", correlationID)
-	ctx = context.WithValue(ctx, "request_id", requestID)
+func MiddlewareIO(ignorePaths ...string) func(c *fiber.Ctx) error {
+	return func(c *fiber.Ctx) error {
+		l := NewLogger()
 
-	l.Log(ctx, slog.LevelInfo, "Request",
-		"endpoint", c.Request().URI().String(),
-		"path", string(c.Request().URI().RequestURI()),
-		"correlation_id", correlationID,
-		"request_id", requestID,
-	)
+		var (
+			correlationID = uuid.New().String()
+			requestID     = uuid.New().String()
+			path          = string(c.Request().URI().Path())
+			endpoint      = c.Request().URI().String()
+		)
 
-	err := c.Next()
-	if err != nil {
-		l.Log(ctx, slog.LevelError, "Response", "correlation_id", correlationID, "request_id", requestID)
-		return err
+		for i := range ignorePaths {
+			if ignorePaths[i] == path {
+				err := c.Next()
+				if err != nil {
+					return err
+				}
+				return nil
+			}
+		}
+
+		ctx := context.WithValue(c.UserContext(), "correlation_id", correlationID)
+		ctx = context.WithValue(ctx, "request_id", requestID)
+
+		l.Log(ctx, slog.LevelInfo, "Request",
+			"endpoint", endpoint,
+			"path", path,
+			"correlation_id", correlationID,
+			"request_id", requestID,
+		)
+
+		err := c.Next()
+		if err != nil {
+			l.Log(ctx, slog.LevelError, "Response", "correlation_id", correlationID, "request_id", requestID)
+			return err
+		}
+
+		l.Log(ctx, slog.LevelError, "Response",
+			"correlation_id", correlationID,
+			"request_id", requestID,
+			"status_code", c.Response().StatusCode(),
+			"body", string(c.Response().Body()),
+			"endpoint", endpoint,
+			"path", path,
+		)
+
+		return nil
 	}
-
-	l.Log(ctx, slog.LevelError, "Response",
-		"correlation_id", correlationID,
-		"request_id", requestID,
-		"status_code", c.Response().StatusCode(),
-		"body", string(c.Response().Body()),
-		"endpoint", c.Request().URI().String(),
-		"path", string(c.Request().URI().RequestURI()),
-	)
-
-	return nil
 }
